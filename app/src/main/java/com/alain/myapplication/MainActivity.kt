94048e2a96d3613c1dd76d1b62cc952f23bb01dd
AB56 <alainbidault56@gmail.com>
2022-04-18 09:29:45 +0200

    Ajouter  intent  pour connexion avec  URL 2 fragments sont générés automatiquement

--- app/src/main/java/com/alain/myapplication/MainActivity.kt
+++ app/src/main/java/com/alain/myapplication/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.alain.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 //testée une fois de plus
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val btnConex: Button = this.findViewById<Button>(R.id.btn_connexion)
+        btnConex.setOnClickListener {
+            val laConnexion: Intent = Intent(this, Connexion::class.java)
+            laConnexion.putExtra("url", "192.168.1.47")
+            val res = startActivity(laConnexion)
+
+        }
     }
 }
