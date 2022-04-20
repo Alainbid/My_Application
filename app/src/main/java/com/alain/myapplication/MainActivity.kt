2100259fb4876da68b28980b9522c851229986cb
AB56 <alainbidault56@gmail.com>
2022-04-20 17:56:18 +0200

    Supprimé le bouton connexion remplacé par un bouton close mesure de la durée du thread

--- app/src/main/java/com/alain/myapplication/MainActivity.kt
+++ app/src/main/java/com/alain/myapplication/MainActivity.kt
@@ -10,9 +10,12 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import kotlin.properties.Delegates
+import kotlin.system.measureTimeMillis
 
 var data: String = "Temperature 00.00 Humidite 00.00 duree 1 frequence 0.00 xxx"
 var runningThread: Boolean = true
+var tps by Delegates.notNull<Long>()
 
 //testée une fois de plus
 class MainActivity : AppCompatActivity() {
@@ -26,42 +29,46 @@ class MainActivity : AppCompatActivity() {
 
         val monThread = Thread {
             val url = URL("http://192.168.1.47/")
-            if (!runningThread) {
-                return@Thread
-            }
-            try {
-                val httpClient = url.openConnection() as HttpURLConnection
-                if (httpClient.responseCode == HttpURLConnection.HTTP_OK) {
-                    try {
-                        val stream = BufferedInputStream(httpClient.inputStream)
-                        readStream(inputStream = stream)
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    } finally {
-                        httpClient.disconnect()
+            tps = measureTimeMillis {
+//            if (!runningThread) {
+//                return@Thread
+//            }
+                try {
+                    val httpClient = url.openConnection() as HttpURLConnection
+                    if (httpClient.responseCode == HttpURLConnection.HTTP_OK) {
+                        try {
+                            val stream = BufferedInputStream(httpClient.inputStream)
+                            readStream(inputStream = stream)
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+
+                        } finally {
+                            httpClient.disconnect()
+                        }
+                    } else {
+                        println("---------------------ERROR ${httpClient.responseCode}")
                     }
-                } else {
-                    println("ERROR ${httpClient.responseCode}")
+                } catch (e: Error) {
+                    println(" --------------------code erreur :$e")
+
+                } finally {
+                    println("--------------------Connexion  terminée")
+                    runningThread = false
+
                 }
-            } catch (e: Error) {
-                println(" code erreur :$e")
-
-            } finally {
-                println("--------------------Connexion OK")
-                runningThread = false
-
             }
         }//*************************fin du Thread
         departThread(monThread)
         // une fois le thread terminé le bouton connexion est visible
 
         while (monThread.isAlive) {
-            println("...")
+            runningThread = true
         }
+        println(" ----------------duree du thread = $tps")
         btnConex.setOnClickListener {
             finishAndRemoveTask()
         }
-        println(" ------------------- alive = " + monThread.isAlive)
+        println(" -------------------thread alive = " + monThread.isAlive)
         var position: List<Int> = data.indexesOf("humidi", true)
         findViewById<TextView>(R.id.tx_humid).text =
             data.substring(position[0], position[0] + 13) + "%"
