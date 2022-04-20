e7d64bd5987c9739f747a4d63cb14b0c5480c32e
AB56 <alainbidault56@gmail.com>
2022-04-20 17:27:58 +0200

    Supprimé le bouton connexion remplacé par un bouton close

--- app/src/main/java/com/alain/myapplication/MainActivity.kt
+++ app/src/main/java/com/alain/myapplication/MainActivity.kt
@@ -2,7 +2,6 @@ package com.alain.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -12,7 +11,9 @@ import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
-var data: String = "Temperature 00.00 Humidite 00.00 duree 1 frequence 0.00 xxxx"
+var data: String = "Temperature 00.00 Humidite 00.00 duree 1 frequence 0.00 xxx"
+var runningThread: Boolean = true
+
 //testée une fois de plus
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
@@ -21,11 +22,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val txduree = findViewById<TextView>(R.id.tx_duree)
         val txfreq = findViewById<TextView>(R.id.tx_freq)
-        val btnConex: Button = this.findViewById<Button>(R.id.btn_connexion)
-        btnConex.visibility = View.GONE
+        val btnConex = this.findViewById<Button>(R.id.btn_connexion)
 
         val monThread = Thread {
             val url = URL("http://192.168.1.47/")
+            if (!runningThread) {
+                return@Thread
+            }
             try {
                 val httpClient = url.openConnection() as HttpURLConnection
                 if (httpClient.responseCode == HttpURLConnection.HTTP_OK) {
@@ -45,35 +48,44 @@ class MainActivity : AppCompatActivity() {
 
             } finally {
                 println("--------------------Connexion OK")
-                btnConex.visibility = View.VISIBLE
+                runningThread = false
+
             }
         }//*************************fin du Thread
         departThread(monThread)
         // une fois le thread terminé le bouton connexion est visible
 
-        btnConex.setOnClickListener {
-            var position: List<Int> = data.indexesOf("humidi", true)
-            findViewById<TextView>(R.id.tx_humid).text =
-                data.substring(position[0], position[0] + 13) + "%"
-            position = data.indexesOf("Temper", true)
-            findViewById<TextView>(R.id.tx_temp).text =
-                data.substring(position[0], position[0] + 16) + " °C"
-
-            position = data.indexesOf("duree ", true)
-            var tx = data.substring(position[0], position[0] + 8)
-            var z = tx.length
-            txduree.text = "Arrosage pendant " + tx.substring(z - 2) + " s"
-
-            position = data.indexesOf("frequence ", true)
-            tx = data.substring(position[0], position[0] + 12)
-            z = tx.length
-            txfreq.text = "toutes les " + tx.substring(z - 3) + " heures"
+        while (monThread.isAlive) {
+            println("...")
         }
+        btnConex.setOnClickListener {
+            finishAndRemoveTask()
+        }
+        println(" ------------------- alive = " + monThread.isAlive)
+        var position: List<Int> = data.indexesOf("humidi", true)
+        findViewById<TextView>(R.id.tx_humid).text =
+            data.substring(position[0], position[0] + 13) + "%"
+        position = data.indexesOf("Temper", true)
+        findViewById<TextView>(R.id.tx_temp).text =
+            data.substring(position[0], position[0] + 16) + " °C"
+
+        position = data.indexesOf("duree ", true)
+        var tx = data.substring(position[0], position[0] + 8)
+        var z = tx.length
+        txduree.text = "Arrosage pendant " + tx.substring(z - 2) + " s"
+
+        position = data.indexesOf("frequence ", true)
+        tx = data.substring(position[0], position[0] + 12)
+        z = tx.length
+        txfreq.text = "toutes les " + tx.substring(z - 3) + " heures"
+
     }
 }
 
 private fun departThread(monThread: Thread) {
     println("---------- debut du thread")
+    println(" -------------------------nom du thread " + monThread.name)
+
     monThread.start()
 }
 
