61df66b3fac2e420538cbb5740d5c8ebcbcb2b5e
AB56 <alainbidault56@gmail.com>
2022-04-21 12:16:17 +0200

    creation et test de la class connecter mais pas d'info si URL  est erronée

--- app/src/main/java/com/alain/myapplication/MainActivity.kt
+++ app/src/main/java/com/alain/myapplication/MainActivity.kt
@@ -6,16 +6,11 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.io.BufferedInputStream
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
 import java.net.URL
-import kotlin.properties.Delegates
-import kotlin.system.measureTimeMillis
+import java.util.Collections.emptySet
 
-var data: String = " "//"Temperature 00.00 Humidite 00.00 duree 1 frequence 0.00 xxx"
-var err: String = " "
+var data: String = ""//"Temperature 00.00 Humidite 00.00 duree 1 frequence 0.00 xxx"
+var err: String = " OK"
 var runningThread = true
 
 //testée une fois de plus
@@ -29,53 +24,10 @@ class MainActivity : AppCompatActivity() {
         val btnClose = this.findViewById<Button>(R.id.btn_connexion)
         val url = URL("http://192.168.1.47/")
 
-        var tps by Delegates.notNull<Long>()
+        Connecter(url).connecter(url)
 
+        Toast.makeText(this, "connexion :  $url  $err", Toast.LENGTH_LONG).show()
 
-        val monThread = Thread {
-
-            tps = measureTimeMillis {
-                try {
-                    val httpClient = url.openConnection() as HttpURLConnection
-
-                    if (httpClient.responseCode == HttpURLConnection.HTTP_OK) {
-                        try {
-                            val stream = BufferedInputStream(httpClient.inputStream)
-                            readStream(inputStream = stream)
-                        } catch (e: Exception) {
-                            err = "pas de readStream"
-                            e.printStackTrace()
-                            println("err $e")
-
-
-                        } finally {
-                            httpClient.disconnect()
-                        }
-                    } else {
-                        err = "pas de connexion"
-                        println("---------------------ERROR ${httpClient.responseCode}")
-                    }
-                } catch (e: Error) {
-                    runningThread = false
-                    println(" --------------------code erreur :$e")
-                    err = e.toString()
-
-
-                } finally {
-                    println("--------------------Connexion  terminée")
-                    runningThread = false
-
-                }
-            }
-        }//*************************fin du Thread
-        departThread(monThread)
-
-        while (monThread.isAlive) {
-            runningThread = true
-        }
-        Toast.makeText(this, "resultat de connexion :  $url  $err", Toast.LENGTH_LONG).show()
-        println(" ----------------duree du thread = $tps")
-        println(" -------------------thread alive = " + monThread.isAlive)
         var position: List<Int> = data.indexesOf("humidi", true)
         findViewById<TextView>(R.id.tx_humid).text =
             data.substring(position[0], position[0] + 13) + "%"
@@ -93,29 +45,13 @@ class MainActivity : AppCompatActivity() {
         z = tx.length
         txfreq.text = "toutes les " + tx.substring(z - 3) + " heures"
 
-
         btnClose.setOnClickListener {
             finishAndRemoveTask()
         }
     }
 }
 
-private fun departThread(monThread: Thread) {
-    println("---------- debut du thread")
-    println(" -------------------------nom du thread " + monThread.name)
 
-    monThread.start()
-}
-
-
-private fun readStream(inputStream: BufferedInputStream) {
-    val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-    val stringBuilder = StringBuilder()
-    bufferedReader.forEachLine { stringBuilder.append(it) }
-
-    data = stringBuilder.toString()
-
-}
 
 //************* recherche des correspondances dans le texte ********************
 private fun String?.indexesOf(pat: String, ignoreCase: Boolean = true): List<Int> =
